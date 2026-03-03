98637462cb449f53df9f0b6a08f51ce0bf138653
Vishal Sharma <vishalimp03@gmail.com>
2026-03-03 15:52:51 +0530

    Intial setup for traffic-control-service

---
+++ src/main/java/com/vishal/traffic_control_service/TrafficControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.traffic_control_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficControlServiceApplication.class, args);
+	}
+
+}
